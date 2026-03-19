47c1b792e510cfc0909a42efa9d990d3a9e03091
Venetia <venetiafaber@gmail.com>
2026-03-19 14:31:30 +0300

    feat: adds Order model and Database Helper

---
+++ lib/db/database_helper.dart
@@ -0,0 +1,111 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/order.dart';
+
+//singleton pattern - ensures only one db connection exists app-wide
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if(_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();     // finds the correct local storage path on the device
+
+    final path = join(dbPath, 'laundry_orders.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,  // runns only on first install
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE orders (
+        ID            INTEGER PRIMARY KEY AUTOINCREMENT,
+        customerName  TEXT    NOT NULL,
+        phoneNumber   TEXT    NOT NULL,
+        serviceType   TEXT    NOT NULL,
+        numberOfItems INTEGER NOT NULL,
+        pricePerItem  REAL    NOT NULL,
+        totalPrice    REAL    NOT NULL,
+        status        TEXT    NOT NULL DEFAULT 'Received'     
+      )
+    '''
+    );
+  }
+
+  // CRUD operations 
+
+  // creates order
+  Future<int> insertOrder(Order order) async {
+    final db = await database;
+    return await db.insert(
+      'orders',       // table name
+      order.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // returns all orders
+  Future<List<Order>> getOrders() async {
+    final db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query(
+      'orders',
+      orderBy: 'id DESC', 
+    );
+
+    return List.generate(maps.length, (i) => Order.fromMap(maps[i]));
+  }
+
+  // updates order status
+  Future<int> updateOrderStatus(int id, String newStatus) async {
+    final db = await database;
+    return await db.update(
+      'orders', 
+      {'status': newStatus},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+  }
+
+  // returns an order(s) / search an order(s)
+  Future<List<Order>> searchOrder(String query) async {
+    final db = await database;
+
+    final String likeQuery = '%$query%';
+
+    final List<Map<String, dynamic>> maps = await db.query(
+      'orders',
+      where: 'customerName LIKE ? OR phoneNumber LIKE ?',
+      whereArgs: [likeQuery, likeQuery],
+      orderBy: 'id DESC',
+    );
+
+    return List.generate(maps.length, (i) => Order.fromMap(maps[i]));
+  }
+
+  // deletes an order
+  Future<int> deleteOrder(int id) async {
+    final db = await database;
+
+    return await db.delete(
+      'orders',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+}
---
+++ lib/models/order.dart
@@ -0,0 +1,72 @@
+class Order {
+  final int? id;
+  final String customerName;
+  final String phoneNumber;
+  final String serviceType;
+  final int numberOfItems;
+  final double pricePerItem;
+  final double totalPrice;    // autocalculated: numberOfItems * pricePerItem
+  final String status;
+
+  // constructor
+  Order({
+    this.id,
+    required this.customerName,
+    required this.phoneNumber,
+    required this.serviceType,
+    required this.numberOfItems,
+    required this.pricePerItem,
+    required this.totalPrice,
+    this.status = 'Received',   // default value
+  });
+
+  // toMap() to raw data / SQLite does not understand Dart ojects, 
+  // convert to key-value maps, id excluded as SQLite auto-generates it
+  Map<String, dynamic> toMap() {
+    return {
+      'customerName': customerName,
+      'phoneNumber': phoneNumber,
+      'serviceType' : serviceType,
+      'numberOfItems': numberOfItems,
+      'pricePerItem': pricePerItem,
+      'totalPrice': totalPrice,
+      'status': status,
+    };
+  }
+
+  // fromMap to object
+  factory Order.fromMap(Map<String, dynamic> map) {
+    return Order(
+      id: map['id'] as int?,
+      customerName: map['customerName'] as String,
+      phoneNumber: map['phoneNumber'] as String,
+      serviceType: map['serviceType'] as String,
+      numberOfItems: map['numberOfItems'] as int,
+      pricePerItem: (map['pricePerItem'] as num).toDouble(),
+      totalPrice: (map['totalPrice'] as num).toDouble(),
+      status: map['status'] as String,
+    );
+  }
+
+  Order copyWith({
+    int? id,
+    String? customerName,
+    String? phoneNumber,
+    String? serviceType,
+    int? numberOfItems,
+    double? pricePerItem,
+    double? totalPrice,
+    String? status,
+  }) {
+    return Order(
+      id: id ?? this.id,
+      customerName: customerName ?? this.customerName,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      serviceType: serviceType ?? this.serviceType,
+      numberOfItems: numberOfItems ?? this.numberOfItems,
+      pricePerItem: pricePerItem ?? this.pricePerItem,
+      totalPrice: totalPrice ?? this.totalPrice,
+      status: status ?? this.status,
+    );
+  }
+}
