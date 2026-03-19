0a18d75bb8db74b203a8fb9e7de3f15e62538b2d
Venetia <venetiafaber@gmail.com>
2026-03-19 13:10:30 +0300

    feat: initial Flutter project setup with folder structure and dependencies

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Laundry Order Management',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Laundry Order Management'),
+          )
+      ),
+    );
+  }
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+// This is a basic Flutter widget test.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:laundry_order_management/main.dart';
+
+void main() {
+}
